e2e81c1b43096bfc5b28e04571cef4706e46b1fe
aparna1208 <119682880+aparna1208@users.noreply.github.com>
2024-05-12 22:31:23 +0530

    Add files via upload

---
+++ about.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def app():
+
+    st.title("WhatsApp Chat Analyzer...")
+    st.write("WhatsApp chat analyzer refers to tools or software that are designed to analyze the data from WhatsApp conversations. These tools can provide various insights into the chats, such as the frequency of messages, most active participants, commonly used words or phrases, sentiment analysis, and more.")
